13adda452fb226d56f8a77b2134380fcf39c9941
kagaro <kagaro@32b681ae-6cbf-4e05-ad6c-3c6d0c131daa>
2009-03-10 14:10:27 +0000

    kgPanels: added a debug flag to us local

--- trunk/kgPanelsConfig/Locales/kgPanels-enUS.lua
+++ trunk/kgPanelsConfig/Locales/kgPanels-enUS.lua
@@ -1,7 +1,12 @@
 ﻿--[[
 Default Locale is English, those providing translations, just add it to the appropiate lua file
 ]]
-local L = LibStub("AceLocale-3.0"):NewLocale("kgPanels","enUS",true)
+local debug = false
+--@debug@
+debug = true
+--@end-debug@
+
+local L = LibStub("AceLocale-3.0"):NewLocale("kgPanels","enUS",debug)
 
 --@localization(locale="enUS", format="lua_additive_table", same-key-is-true=true, table-name="L")@
 
