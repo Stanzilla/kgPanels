66c893815d83fa717cf55afe360f0fa04e7ecc8d
kagaro <kagaro@32b681ae-6cbf-4e05-ad6c-3c6d0c131daa>
2009-03-14 03:11:21 +0000

    kgPanels: fix for debug values

--- trunk/kgPanelsConfig/Locales/kgPanels-enUS.lua
+++ trunk/kgPanelsConfig/Locales/kgPanels-enUS.lua
@@ -1,12 +1,8 @@
 ﻿--[[
 Default Locale is English, those providing translations, just add it to the appropiate lua file
 ]]
-local debug = false
---@debug@
-debug = true
---@end-debug@
 
-local L = LibStub("AceLocale-3.0"):NewLocale("kgPanels","enUS",debug)
+local L = LibStub("AceLocale-3.0"):NewLocale("kgPanels","enUS",true)
 
 --@localization(locale="enUS", format="lua_additive_table", same-key-is-true=true, table-name="L")@
 
