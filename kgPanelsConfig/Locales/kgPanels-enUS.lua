88e850bb91ae341b75377029b3b38e28432c83f2
durcyn <373838+durcyn@users.noreply.github.com>
2009-05-05 17:17:13 +0000

    prevent locale errors on svn checkouts

--- kgPanelsConfig/Locales/kgPanels-enUS.lua
+++ kgPanelsConfig/Locales/kgPanels-enUS.lua
@@ -2,7 +2,12 @@
 Default Locale is English, those providing translations, just add it to the appropiate lua file
 ]]
 
-local L = LibStub("AceLocale-3.0"):NewLocale("kgPanels","enUS",true)
+local debug = false
+--@debug@
+debug = true
+--@end-debug@
+
+local L = LibStub("AceLocale-3.0"):NewLocale("kgPanels","enUS",true, debug)
 
 --@localization(locale="enUS", format="lua_additive_table", same-key-is-true=true, table-name="L")@
 
