Stanzilla/kgPanels

commit 91ff84ee9a02f181e395e1a1d2c52902adfa3d6e

don't error on WoW Classic

kgPanels.lua
@@ -4,6 +4,11 @@ All Hello Kitty intellectual property and materials are © Sanrio
 local kgPanels = LibStub("AceAddon-3.0"):NewAddon("kgPanels", "AceConsole-3.0")
 local LSM = LibStub:GetLibrary("LibSharedMedia-3.0",true)
 local BD = LibStub:GetLibrary("LibBackdrop-1.0",true)
+
+local function IsClassic()
+	return WOW_PROJECT_ID == WOW_PROJECT_CLASSIC
+end
+
 --[[
 	some defaults
 ]]
@@ -432,9 +437,11 @@ local launcher
 function kgPanels:OnInitialize()
 	self.db = LibStub("AceDB-3.0"):New("kgPanelsDB", dbDefaults, "Default")
 	self:UpgradeDB()
-	local LibDualSpec = LibStub('LibDualSpec-1.0',true)
-	if LibDualSpec then
-		LibDualSpec:EnhanceDatabase(self.db, "kgPanels")
+	if not IsClassic then
+		local LibDualSpec = LibStub('LibDualSpec-1.0',true)
+		if LibDualSpec then
+			LibDualSpec:EnhanceDatabase(self.db, "kgPanels")
+		end
 	end
 	self:RegisterChatCommand("kgPanels","CommandLine",true,true)
 	self.parents = parents

kgPanelsConfig/kgPanelsConfig.lua
@@ -17,6 +17,10 @@ local L = LibStub("AceLocale-3.0"):GetLocale("kgPanels", false)
 local cfgreg = LibStub("AceConfigRegistry-3.0")
 local serializer = LibStub("AceSerializer-3.0")
 
+local function IsClassic()
+	return WOW_PROJECT_ID == WOW_PROJECT_CLASSIC
+end
+
 local _assetName,_assetPath,_assetType = nil,nil,"artwork"
 local _panelName = nil
 local _layoutName = nil
@@ -597,9 +601,11 @@ function kgPanelsConfig:OnInitialize()
 	-- Register our option table
 	optionTable = self:makeOptions()
 	optionTable.args.profile = LibStub("AceDBOptions-3.0"):GetOptionsTable(kgPanels.db)
-	local LibDualSpec = LibStub('LibDualSpec-1.0')
-	if LibDualSpec then
-		LibDualSpec:EnhanceOptions(optionTable.args.profile, kgPanels.db)
+	if not IsClassic() then
+		local LibDualSpec = LibStub('LibDualSpec-1.0')
+		if LibDualSpec then
+			LibDualSpec:EnhanceOptions(optionTable.args.profile, kgPanels.db)
+		end
 	end
 	LibStub("AceConfig-3.0"):RegisterOptionsTable("kgPanelsConfig", optionTable)
 	-- LAYOUT MENU SHORTCUTS

commit eb53b09503fde90db2134a87829f2fdbe237fbcb

Fix kgPanels:OnInitialize()
Commit 91ff84ee9a02f181e395e1a1d2c52902adfa3d6e introduced a bug causing kgpanels to not correctly initialize on WoW Retail.
In opposition to kgPanelsConfig\kgPanelsConfig.lua the function was evaluated instead of the true/false return.
This commit fix this bug and permit KgPanels to work on Retail's version

Typical InGame StackTrace :
3x kgPanelsConfig\kgPanelsConfig-1.23.lua:607: Usage: LibDualSpec:EnhanceOptions(optionTable, target): EnhanceDatabase should be called before EnhanceOptions(optionTable, target).
[C]: in function `error'
...Ons\Bazooka\libs\LibDualSpec-1.0\LibDualSpec-1.0-17.lua:348: in function `EnhanceOptions'
kgPanelsConfig\kgPanelsConfig-1.23.lua:607: in function <kgPanelsConfig\kgPanelsConfig.lua:596>
[C]: ?
...ceAdiBags\libs\AceAddon-3.0\AceAddon-3.0-12.lua:70: in function <...ceAdiBags\libs\AceAddon-3.0\AceAddon-3.0.lua:65>
...ceAdiBags\libs\AceAddon-3.0\AceAddon-3.0-12.lua:498: in function `InitializeAddon'
...ceAdiBags\libs\AceAddon-3.0\AceAddon-3.0-12.lua:613: in function <...ceAdiBags\libs\AceAddon-3.0\AceAddon-3.0.lua:605>
[C]: in function `LoadAddOn'
kgPanels\kgPanels-1.23.lua:634: in function `?'
...ddOns\AdiBags\libs\AceConsole-3.0\AceConsole-3.0-7.lua:94: in function `?'
FrameXML\ChatFrame.lua:4836: in function <FrameXML\ChatFrame.lua:4783>
[C]: in function `ChatEdit_ParseText'
FrameXML\ChatFrame.lua:4497: in function <FrameXML\ChatFrame.lua:4496>
[C]: in function `ChatEdit_SendText'
FrameXML\ChatFrame.lua:4533: in function `ChatEdit_OnEnterPressed'
[string "*:OnEnterPressed"]:1: in function <[string "*:OnEnterPressed"]:1>

3x kgPanelsConfig\ArtHelper.lua:173: attempt to index field 'artLibrary' (a nil value)
kgPanelsConfig\ArtHelper.lua:173: in function `CreateArtMenu'
kgPanelsConfig\ArtHelper.lua:13: in function `InitArt'
kgPanelsConfig\kgPanelsConfig-1.23.lua:637: in function <kgPanelsConfig\kgPanelsConfig.lua:635>
[C]: ?

kgPanels.lua
@@ -437,7 +437,7 @@ local launcher
 function kgPanels:OnInitialize()
 	self.db = LibStub("AceDB-3.0"):New("kgPanelsDB", dbDefaults, "Default")
 	self:UpgradeDB()
-	if not IsClassic then
+	if not IsClassic() then
 		local LibDualSpec = LibStub('LibDualSpec-1.0',true)
 		if LibDualSpec then
 			LibDualSpec:EnhanceDatabase(self.db, "kgPanels")